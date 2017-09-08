a454d41cb475dd9b0870c3e7f4a69c0d9286f1bb
magicisland <marcelojavierrodriguez@gmail.com>
2017-09-08 04:32:54 -0300

    ..

---
+++ src/main/java/hbannotation_2/many_to_one/App.java
@@ -0,0 +1,13 @@
+package hbannotation_2.many_to_one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
